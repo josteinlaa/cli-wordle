b4eab2798065aabbc5e7f7c6a9a806a644dbf362
jostein lind <jostelaa@stud.ntnu.no>
2024-09-28 15:39:07 +0200

    codeql2

--- src/main/java/no/jostein/game/Game.java
+++ src/main/java/no/jostein/game/Game.java
@@ -3,21 +3,26 @@ package no.jostein.game;
 import java.util.Scanner;
 
 import no.jostein.service.GameHandler;
-import no.jostein.ui.GameRenderer;
+import no.jostein.ui.CLIUserInterface;
+import no.jostein.ui.IUserInterface;
 
 public class Game {
-    public void runGame() {
-        GameHandler gh = new GameHandler();
-        GameRenderer gr = new GameRenderer();
-        Scanner scanner = new Scanner(System.in);
-        String guess;
+    private GameHandler gameHandler;
+    private IUserInterface userInterface;
 
-      
+    public Game(GameHandler gameHandler, IUserInterface userInterface) {
+        this.gameHandler = gameHandler;
+        this.userInterface = userInterface;
+    }
+
+    public void runGame() {
+        CLIUserInterface gr = new CLIUserInterface();
+        String guess;
 
         while (true) {
             System.out.println("NEW GAME. Guess the word:");
             for (int i = 0; i < 5; i++) {
-                guess = scanner.nextLine();
+                guess = gr.getUserGuess();
                 gh.makeGuess(guess, i);
                 gr.clearTerminal();
                 gr.printTurn(gh.getHistory());
